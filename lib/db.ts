4c960d1d0faeb0589016e0499707729a52a49201
sheikhsarowar <sttechbdsheikhsarowarislam@gmail.com>
2026-07-26 15:30:36 +0600

    Initial clean commit

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,16 @@
 import mysql from 'mysql2/promise';
 
 export const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT) || 4000, // TiDB এর জন্য ৪০০০ পোর্ট
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+  ssl: {
+    minVersion: 'TLSv1.2',
+    rejectUnauthorized: true
+  }
 });
